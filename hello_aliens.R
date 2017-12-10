2f007ec0169e055397dacc4461fd8fd6610a5668
Amy Fan <ayf@andrew.cmu.edu>
2017-12-10 15:22:07 -0500

    Hello Aliens!

---
+++ hello_aliens.R
@@ -0,0 +1,12 @@
+cat("Hello intelligent extraterrestrials!\n
+My name is Amy Fan, and I am a student at Carnegie Mellon University (or a human from planet Earth).\n
+I recently discovered your planet and I'm curious to learn more about your kind and your world.\n
+Earth is an intriguing and magnificent place, and I wonder if your world is comparable!\n
+Some questions I have for you:\n
+What kind of food do you eat?\n
+How do you spend your days?\n
+How do you communicate with others?\n
+What kinds of rituals or traditions do you follow?\n
+I know this may be overwhelming, but I'm eager to hear back.\n
+I'd also be happy to answer any questions you have about what it's like on Earth.\n
+Thanks!")
